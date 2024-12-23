ac3e07a773bbd39f7e713c04ed19e4f4c6791bf2
Justinbeer <96567017+Justinbeer@users.noreply.github.com>
2024-12-23 16:40:13 +0900

    임시테스트

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
 // import App from "../src/styled/main.css";
-import App from "../src/pay/App.jsx";
+// import App from "../src/pay/App.jsx";
+import App from "/src/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/pay/App.jsx
+++ src/pay/App.jsx
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
-<body>
-    <button>결제</button>
-</body>
-</html>
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>Document</title>
+// </head>
+// <body>
+//     <button>결제</button>
+// </body>
+// </html>
