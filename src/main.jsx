b68028418d270a73b5ff4c15cbf2ec5940ad6844
KimGyeongBln <hhk3950@naver.com>
2024-12-24 09:46:13 +0900

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import "./styled/Modal.css";
-// import App from "./app/Modal";
+// import App from "./app/Modal"; 이렇게 2줄 주석
 // import App from "./components/toppingslide";
 // import App from "/src/app/mainHome.jsx";
 import App from "./App";
