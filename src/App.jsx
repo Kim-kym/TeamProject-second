f9a1028a9897890771b7c33216aa37aa30ec246e
Justinbeer <96567017+Justinbeer@users.noreply.github.com>
2024-12-27 13:39:57 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 //test code
 import Home from "./components/김경빈/Home";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Menu from "./components/김경빈/Menu";
-=======
-import Menu from "./components/김경빈/Menu"
->>>>>>> bin
-// import MainHome from "./app/MainHome";
-=======
-import MainHome from "./app/mainHome";
->>>>>>> yyj2
-// import Topping from "./app/Modal";
+// import Menu from "./components/김경빈/Menu"
+import MainHome from "./app/MainHome";
+// import MainHome from "./app/mainHome";
+import Topping from "./app/Modal";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
