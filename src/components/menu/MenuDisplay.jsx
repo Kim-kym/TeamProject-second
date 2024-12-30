b8030780c6062bbbb9b65136ffd23bef64242b0c
YuYeaJin <yyj9290@naver.com>
2024-12-30 09:39:35 +0900

    메뉴 리스트 Reack-Slick 설정

--- src/components/menu/MenuDisplay.jsx
+++ src/components/menu/MenuDisplay.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import Category from "./Category";
 import MenuList from "./MenuList";
+import Slider from "react-slick";
 
 function MenuDisplay({
   currentMenu,
@@ -9,15 +10,32 @@ function MenuDisplay({
   handleMenuClick,
   menuListData,
 }) {
+  const slickRef = useRef(null);
+
+  const previous = useCallback(() => slickRef.current.slickPrev(), []);
+  const next = useCallback(() => slickRef.current.slickNext(), []);
+
+  const settings = {
+    dots: true,
+    infinite: false, // 무한 슬라이드 비활성화
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    rows: 2,
+    swipeToSlide: true,
+    arrows: false,
+  };
+
   return (
     <div className="menu-display">
       {/* 카테고리 버튼 */}
       <Category setCurrentMenu={setCurrentMenu} />
-      <MenuList
-        menuData={menuListData[currentMenu]}
-        addToCart={addToCart}
-        handleMenuClick={handleMenuClick}
-      />
+      <Slider {...settings} ref={slickRef}>
+        <MenuList
+          menuData={menuListData[currentMenu]}
+          addToCart={addToCart}
+          handleMenuClick={handleMenuClick}
+        />
+      </Slider>
     </div>
   );
 }
