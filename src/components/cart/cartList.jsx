90ad64c274365cdac4f798ef041814ab8d8da81c
Justinbeer <96567017+Justinbeer@users.noreply.github.com>
2025-01-03 10:21:37 +0900

    장바구니css수정

--- src/components/cart/cartList.jsx
+++ src/components/cart/cartList.jsx
@@ -32,6 +32,16 @@ function CartList({ cart, removeFromCart, updateQuantity }) {
     0
   );
 
+  // 결제 처리 함수
+  const handlePayment = () => {
+    setPaymentCompleted(true); // 결제 완료 상태 변경
+    setIsOpen(false); // 장바구니 닫기
+  };
+
+  const toggleCart = () => {
+    setIsOpen(!isOpen); // 장바구니 열기/닫기 상태 변경
+  };
+
   return (
     <div id="cart">
       <h2>장바구니</h2>
@@ -124,5 +134,4 @@ function CartList({ cart, removeFromCart, updateQuantity }) {
     </div>
   );
 }
-
 export default CartList;
